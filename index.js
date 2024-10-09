d30781c78998e95646fb1058cb8e1eb122dc0e86
victor <mwakiretivictor@gmail.com>
2024-10-09 15:06:04 +0300

    update my looping code along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; // Create a new, empty array to hold the messages
+  
+    for (let i = 0; i < names.length; i++) {
+      // Create a custom message for each name
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  
+      // Add the message to the new array
+      messages.push(message);
+    }
+  
+    // Return the array of messages after the loop finishes
+    return messages;
+  }  
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--; // Decrease the number by 1 in each iteration
+    }
+  }
+ countDown(10);  
